bd8765315d78b9d0e455d63061e8472dee3eaab5
Warathep Padungtin <48309588+Warathep@users.noreply.github.com>
2025-07-26 18:24:25 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+let qrt = prompt("จำนวนสินค้า")
+let sum = 0
+for(var i = 1; i <= qrt; i++)
+{
+    let item_price = prompt("ราคาสินค้าชิ้นที่" + i)
+    document.getElementById("price-list").innerHTML += 
+    "รายการสินค้าชิ้นที่ " + i + " : " + item_price + " บาท" + "<br>"
+    sum += parseInt(item_price)
+}
+
+document.getElementById("result").innerHTML = "ราคารวม " + sum + " บาท"
+console.log(sum)
